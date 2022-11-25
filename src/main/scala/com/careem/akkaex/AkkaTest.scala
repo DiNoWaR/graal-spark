c2b1f35e20cf2625f1958473588a3675791decc8
denis.vasilev <denis.vasilev@careem.com>
2022-11-25 22:13:42 +0400

    fisrt commit

--- src/main/scala/com/careem/akkaex/AkkaTest.scala
+++ src/main/scala/com/careem/akkaex/AkkaTest.scala
@@ -2,6 +2,8 @@ package com.careem.akkaex
 
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorSystem, Behavior}
+import scala.concurrent.ExecutionContext.Implicits.global
+import java.time.Duration
 
 object AkkaTest {
 
@@ -12,15 +14,19 @@ object AkkaTest {
       Behaviors.receiveMessage {
         case EatChocolate =>
           println(s"($happiness) Eating chocolate, getting a shot of dopamine!")
-          happiness += 1
+          happiness += 2
           Behaviors.same
         case WashDishes =>
-          println(s"($happiness) Doing chores, womp, womp...")
-          happiness -= 2
+          if (happiness <= 0) {
+            println(s"Sorry, bad Mood for dish. Happinnes is $happiness")
+          } else {
+            println(s"($happiness) Doing chores, womp, womp...")
+            happiness -= 6
+          }
           Behaviors.same
         case LearnAkka =>
           println(s"($happiness) Learning Akka, looking good!")
-          happiness += 100
+          happiness += 4
           Behaviors.same
         case _ =>
           println(s"($happiness) Received something i don't know")
@@ -29,13 +35,11 @@ object AkkaTest {
     }
 
     val emotionalActorSystem = ActorSystem(emotionalMutableActor, "EmotionalSystem")
+    val scheduler = emotionalActorSystem.scheduler
 
-    emotionalActorSystem ! EatChocolate
-    emotionalActorSystem ! EatChocolate
-    emotionalActorSystem ! WashDishes
-    emotionalActorSystem ! LearnAkka
-
-    emotionalActorSystem.terminate()
+    scheduler.scheduleAtFixedRate(Duration.ZERO, Duration.ofMillis(700), () => emotionalActorSystem ! EatChocolate, global)
+    scheduler.scheduleAtFixedRate(Duration.ZERO, Duration.ofMillis(300), () => emotionalActorSystem ! WashDishes, global)
+    scheduler.scheduleAtFixedRate(Duration.ZERO, Duration.ofMillis(2300), () => emotionalActorSystem ! LearnAkka, global)
   }
 }
 
